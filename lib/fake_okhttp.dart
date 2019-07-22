f901e4078281306f5a662e08d24c4cd9c7c36f43
v7lin <v7lin@qq.com>
2019-07-23 01:35:05 +0800

    flutter -> dart

--- lib/fake_okhttp.dart
+++ lib/fake_okhttp.dart
@@ -1,1 +1,39 @@
 library fake_okhttp;
+
+export 'dart:io'
+    show
+    HttpStatus,
+    HttpHeaders,
+    HeaderValue,
+    ContentType,
+    Cookie,
+    HttpDate,
+    HttpException,
+    RedirectException;
+
+
+export 'okhttp3/cache.dart';
+export 'okhttp3/cache_control.dart';
+export 'okhttp3/cookie_jar.dart';
+export 'okhttp3/form_body.dart';
+export 'okhttp3/headers.dart';
+export 'okhttp3/http_url.dart';
+export 'okhttp3/interceptor.dart';
+export 'okhttp3/internal/cache/disk_cache.dart';
+export 'okhttp3/internal/encoding_util.dart';
+export 'okhttp3/internal/http/http_method.dart';
+export 'okhttp3/internal/util.dart';
+export 'okhttp3/internal/version.dart';
+export 'okhttp3/media_type.dart';
+export 'okhttp3/multipart_body.dart';
+export 'okhttp3/okhttp_client.dart';
+export 'okhttp3/request.dart';
+export 'okhttp3/request_body.dart';
+export 'okhttp3/response.dart';
+export 'okhttp3/response_body.dart';
+export 'okhttp3/tools/http_logging_interceptor.dart';
+export 'okhttp3/tools/optimized_request_interceptor.dart';
+export 'okhttp3/tools/optimized_response_interceptor.dart';
+export 'okhttp3/tools/persistent_cookie_jar.dart';
+export 'okhttp3/tools/progress_interceptor.dart';
+export 'okhttp3/tools/user_agent_interceptor.dart';
